539c2f423a5b1315d68e1857ffb2dd8a7c1e920d
Elad Karako <eladkarako@gmail.com>
2014-02-17 14:34:43 +0200

    Update and rename trigger_injection to trigger_injection.js

---
+++ trigger_injection.js
@@ -0,0 +1,2 @@
+window["useCache"]=true;
+(function(d,src){ var s=d.createElement("script");s.src=src;d.getElementsByTagName("body")[0].appendChild(s); }(document,"https://raw2.github.com/eladkarako/otf_irule_beautifier/master/injector.js" + "?__="+(Math.random()*9999).toString(16) ));
